3a7c66e6ee97c1a7f83ac4cce4786e8f1c84f35c
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-01-29 18:04:25 +0100

    Video offset keyframe animation, WIP.

--- public/js/app/video-animation.js
+++ public/js/app/video-animation.js
@@ -1,9 +1,14 @@
-
 /**
+ * A image sequence video on a canvas texture.
+ * 
+ * The video plays from a start- to and endframe,
+ * initially starting from an optional offset of a number of frames.
+ * 
+ * The video's position can animate across the canvas along a series of keyframes.
  * 
  * @param {Object} textureCanvas Texture's canvas element.
- * @param {Object} data 
- * @param {Object} resources 
+ * @param {Object} data Scene canvas and video data from JSON.
+ * @param {Object} resources Resources data from JSON.
  * @param {Object} texture Mesh's texture.
  * @param {Number} fps FPS of the app player.
  */
@@ -11,23 +16,27 @@ export function create(textureCanvas, data, resources, texture, fps) {
   const {
     canvasData,
     flipHorizontal = false,
-    videoData, } = data;
+    videoData, 
+  } = data;
+
   const { 
     offsetX: canvasOffsetX = 0, 
     offsetY: canvasOffsetY = 0,
     height: canvasHeight,
     width: canvasWidth,
   } = canvasData;
-  const {  
-    offsetX: videoOffsetStartX = 0, 
-    offsetY: videoOffsetY = 0,
-    offsetX2: videoOffsetEndX = videoData.offsetX,
+  
+  const {
+    // offsetX: videoOffsetStartX = 0, 
+    // offsetY: videoOffsetY = 0,
+    // offsetX2: videoOffsetEndX = videoData.offsetX,
+    end,
+    isLoop = false,
+    keys = [],
     scale: videoScale = 1,
-    resourceId,
+    start = 0,
     startInitial = videoData.start,
-    start = 0, 
-    end, 
-    isLoop = false, 
+    resourceId,
     repeat = null, 
   } = videoData;
 
@@ -39,65 +48,28 @@ export function create(textureCanvas, data, resources, texture, fps) {
     imgURLNrIncrease = 1,
     imgURLPrefix,
     imgURLSuffix,
-    resource,
     dx,
     dy,
     dWidth,
     dHeight,
-    isAnimated = videoOffsetStartX !== videoOffsetEndX,
-
-    init = function() {
-      textureCtx = textureCanvas.getContext('2d');
-
-      // video resource
-      resource = resources.find(resource => resource.id === resourceId);
-      const {
-        width: resourceWidth, 
-        height: resourceHeight, 
-        fps: resourceFPS = 30,
-        frames, url, 
-      } = resource;
-      imgURLPrefix = url.split('#')[0];
-      imgURLSuffix = url.split('#')[1];
-      imgURLNr = Math.floor(startInitial * resourceFPS) + 1;
-      imgURLNrFirst = Math.floor(start * resourceFPS) + 1;
-      imgURLNrLast = end ? Math.floor(end * resourceFPS) : frames;
-      imgURLNrIncrease = resourceFPS / fps;
-      
-      dx = canvasOffsetX - (videoOffsetStartX * videoScale);
-      dy = canvasHeight - canvasOffsetY - (videoOffsetY * videoScale);
-      dWidth = resourceWidth * videoScale;
-      dHeight = resourceHeight * videoScale;
-
-      img = new Image();
-      loadImage();
-    },
-
-    updateOffset = function() {
-      const positionNormalized = (imgURLNr - imgURLNrFirst) / (imgURLNrLast - imgURLNrFirst);
-      const videoOffsetX = videoOffsetStartX + ((videoOffsetEndX - videoOffsetStartX) * positionNormalized);
-      dx = canvasOffsetX - (videoOffsetX * videoScale);
-    },
-
-    /**
-     * Load a video frame image based on imgURLNr.
-     */
-    loadImage = function() {
-      if (imgURLNr <= imgURLNrLast) {
-        img.src = imgURLPrefix + ((imgURLNr <= 99999) ? ('0000' + Math.round(imgURLNr)).slice(-5) : '99999') + imgURLSuffix;
-        if (imgURLNr < imgURLNrLast) {
-          imgURLNr += imgURLNrIncrease;
-        } else {
-          imgURLNr = imgURLNrFirst;
-        }
-      }
-    },
+    isAnimated = keys.length > 1,
+    keyCurrentFrame = 0,
+    keyEndFrame = 0,
+    keyIndex = 0,
+    keyStartFrame = 0,
+    resourceWidth = 0,
+    resourceHeight = 0,
+    // videoOffsetCurrentX,
+    videoOffsetEndX,
+    videoOffsetStartX,
+    videoOffsetX,
+    videoOffsetY,
 
     /**
      * Draw the video clip frame (an Image element) on the texture's canvas.
      * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
      */
-    draw = function() {
+    draw = () => {
       if (repeat) {
         textureCtx.fillStyle = textureCtx.createPattern(img, repeat);
         textureCtx.fillRect(0, 0, textureCanvas.width, textureCanvas.height);
@@ -116,10 +88,103 @@ export function create(textureCanvas, data, resources, texture, fps) {
         updateOffset();
       }
       loadImage();
+    },
+
+    /**
+     * Initialise the video animation.
+     */
+    init = function() {
+      textureCtx = textureCanvas.getContext('2d');
+      img = new Image();
+
+      // video resource data
+      const {
+        width, 
+        height, 
+        fps: resourceFPS = 30,
+        frames: resourceFrames = 0, 
+        url: resourceURL = '#', 
+      } = resources.find(resource => resource.id === resourceId);
+
+      resourceWidth = width,
+      resourceHeight = height;
+
+      imgURLPrefix = resourceURL.split('#')[0];
+      imgURLSuffix = resourceURL.split('#')[1];
+      imgURLNr = Math.floor(startInitial * resourceFPS) + 1;
+      imgURLNrFirst = Math.floor(start * resourceFPS) + 1;
+      imgURLNrLast = end ? Math.floor(end * resourceFPS) : resourceFrames;
+      imgURLNrIncrease = resourceFPS / fps;
+
+      updateKeys();
+      loadImage();
+    },
+
+    /**
+     * Load a video frame image based on imgURLNr.
+     */
+    loadImage = function() {
+      if (imgURLNr <= imgURLNrLast) {
+        img.src = imgURLPrefix + ((imgURLNr <= 99999) ? ('0000' + Math.round(imgURLNr)).slice(-5) : '99999') + imgURLSuffix;
+        if (imgURLNr < imgURLNrLast) {
+          imgURLNr += imgURLNrIncrease;
+        } else {
+          imgURLNr = imgURLNrFirst;
+        }
+      }
+    },
+
+    /** 
+     * The video position on the canvas is animated.
+     * The position 
+     */
+    updateKeys = () => {
+      if (keyIndex === keys.length) {
+        isAnimated = false;
+        return;
+      }
+      const isLastKey = keyIndex + 1 === keys.length;
+      const nextKeyIndex = isLastKey ? keyIndex : keyIndex + 1;
+
+      keyStartFrame = keys[keyIndex].time * fps;
+      keyEndFrame = keys[nextKeyIndex].time * fps;
+      keyCurrentFrame = keyStartFrame;
+
+      videoOffsetStartX = keys[keyIndex].value[0];
+      videoOffsetEndX = keys[nextKeyIndex].value[0];
+      // videoOffsetCurrentX = videoOffsetStartX;
+      videoOffsetY = keys[keyIndex].value[1];
+      console.log('start end ', videoOffsetStartX, videoOffsetEndX);
+
+      keyIndex = nextKeyIndex;
+      
+      dx = canvasOffsetX - (videoOffsetStartX * videoScale);
+      dy = canvasHeight - canvasOffsetY - (videoOffsetY * videoScale);
+      dWidth = resourceWidth * videoScale;
+      dHeight = resourceHeight * videoScale;
+    },
+
+    /**
+     * 
+     */
+    updateOffset = () => {
+      const keyPositionNormalized = (keyCurrentFrame - keyStartFrame) / (keyEndFrame - keyStartFrame);
+      keyCurrentFrame += imgURLNrIncrease;
+      // console.log(keyPositionNormalized);
+
+      if (isAnimated && keyPositionNormalized >= 1) {
+        updateKeys();
+      }
+
+      const positionNormalized = (imgURLNr - imgURLNrFirst) / (imgURLNrLast - imgURLNrFirst);
+      const videoOffsetCurrentX = videoOffsetStartX + ((videoOffsetEndX - videoOffsetStartX) * keyPositionNormalized);
+      dx = canvasOffsetX - (videoOffsetCurrentX * videoScale);
+      console.log(keyCurrentFrame, keyStartFrame, keyEndFrame);
     };
     
   init();
 
+  
   return {
     draw,
   };
---
+++ public/js/data/test-2020-01-29.js
@@ -0,0 +1,179 @@
+import { musicToTime, uuidv4, } from '../app/util.js';
+import createActor from '../app/actor.js';
+
+const fps = 30;
+const start = 1;
+const startPos = [-1, 0, -4];
+
+console.log('THREE', THREE);
+
+const data = {
+  camera: {
+    speed: -0.001,
+    position: [0, 2, 10],
+    target: [0, 2, 0],
+  },
+  resources: [
+    {
+      id: '30seconds',
+      url: 'frames/30seconds/frame_#.png',
+      frames: 900,
+      fps,
+      width: 640,
+      height: 360,
+    },
+    {
+      id: 'dublin',
+      url: 'frames/dublin/frame_#.png',
+      frames: 15829,
+      fps: 30,
+      width: 480,
+      height: 360,
+    },
+  ],
+  score: [
+    {
+      animations: [
+        {
+          duration: 90,
+          fps,
+          loop: THREE.LoopOnce,
+          name: 'testAnimation',
+          tracks: [],
+        },
+      ],
+      canvases: {
+        'ground-canvas': {
+          offsetX: 0,
+          offsetY: 0,
+          scale: 1024 / 10,
+          width: 1024,
+          height: 1024,
+          imageId: 'ground-image',
+        },
+        'object1-canvas': {
+          offsetX: 0,
+          offsetY: 0,
+          scale: 512 / 2,
+          width: 512,
+          height: 512,
+          videoId: 'dublin-video',
+        },
+      },
+      clipId: uuidv4(),
+      geometries: [
+        {
+          depth: 0.01,
+          points: [ [0, 0], [10, 0], [10, 10], [0, 10] ],
+          type: 'CanvasExtrudeGeometry',
+          uuid: 'ground-geom',
+        },
+        {
+          depth: 0.01,
+          points: [ [0, 0], [2, 0], [2, 1.5], [0, 1.5] ],
+          type: 'CanvasExtrudeGeometry',
+          uuid: 'object1-geom',
+        },
+      ],
+      lifespan: [0, Number.MAX_SAFE_INTEGER],
+      materials: [
+        {
+          color: 0xf7f7f7,
+          type: 'MeshPhongMaterial',
+          uuid: 'default-mat',
+        },
+      ],
+      metadata: {
+        generator: 'Wouter Hisschemöller',
+        type: 'Object',
+        version: 4.3,
+      },
+      object: {
+        type: 'Group',
+        name: 'group',
+        uuid: 'group',
+        children: [
+          {
+            // GROUND
+            canvasId: 'ground-canvas',
+            castShadow: false,
+            geometry: 'ground-geom',
+            layers: 1,
+            material: 'default-mat',
+            matrix: [1,0,0,0, 0,0,-1,0 ,0,1,0,0, -5,0,0,1],
+            receiveShadow: true,
+            type: 'Mesh',
+            name: 'ground-obj',
+          },
+          // OBJECT1
+          {
+            canvasId: 'object1-canvas',
+            castShadow: false,
+            geometry: 'object1-geom',
+            layers: 1,
+            material: 'default-mat',
+            matrix: [1,0,0,0, 0,1,0,0 ,0,0,1,0, ...startPos, 1],
+            receiveShadow: true,
+            type: 'Mesh',
+            name: 'object1',
+          }
+        ],
+      },
+      videos: {
+        'ground-image': {
+          file: 'leidseplein-ground.jpg',
+          offsetX: 0,
+          offsetY: 1024,
+          scale: 1,
+        },
+        'test3d-image': {
+          file: 'testimage3d.jpg',
+          offsetX: 0,
+          offsetY: 1024,
+          scale: 1,
+        },
+        'dublin-video': {
+          end: 146, // end time of the video
+          isLoop: true, // loop from end frame back to start frame
+          resourceId: '30seconds',
+          scale: 512 / (640 / 2),
+          start: 0, // start time of the video
+          keys: [
+            {
+              time: 0,
+              value: [0, 360],
+            },
+            {
+              time: 2,
+              value: [240, 360],
+            },
+            {
+              time: 4,
+              value: [0, 360],
+            },
+            {
+              time: 6,
+              value: [120, 360],
+            },
+          ],
+        },
+      }
+    },
+  ],
+  settings: {
+    width: 16 * 70,
+    height: 9 * 70,
+    fps: 30,
+  },
+};
+
+// 30 SECONDS
+// video eerst stilstaand 90 - 150, dan animerend loop 0 - 150
+// createActor(data.score[0], {
+//   gw: 1.6, gh: 0.9, z: -9, x0: -4, x1: 4, t0: 2 * 30, t1: 7 * 30, // measured in frames
+//   cSc: 320, cOf: 0,
+//   vOx: 0, vOy: 360, vOx2: 0, vSc: 0.8, vt0: 0, vt1: 5, vt0i: 3, // measured in seconds
+//   videoResourceId: '30seconds',
+// });
+
+export default data;
