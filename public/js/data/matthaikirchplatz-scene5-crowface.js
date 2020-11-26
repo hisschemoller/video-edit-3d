c5338b10a25f0aac7ac4ce6b1bf7121ffe3c9199
Wouter Hisschemöller <email@hisschemoller.com>
2020-11-26 20:46:03 +0100

    Multiple cylinders.

--- public/js/data/matthaikirchplatz-scene5-crowface.js
+++ public/js/data/matthaikirchplatz-scene5-crowface.js
@@ -7,53 +7,66 @@ const scene = getDefaultScene([ 0 /* 35 */, 600], 5, true);
 export default scene;
 
 // CYLINDER
-const id = uuidv4();
-const data = {
-  id,
-  imageFile: 'testimage3d.jpg',
-  keys: [
-    { time:   0, value: [-9.5, 4, -5]},
-    { time:  30, value: [11, 4, -5]},
-  ],
-  modelFile: 'matthaikirchplatz.glb',
-  modelName: 'cylinder',
-}
-scene.external3DModels.push(data);
+createCylinder({x1: -9.0, x2: 11, y: 4, z: -5, time1: 0, time2: 30, 
+  rotateDuration: 2});
+createCylinder({x1: -11.0, x2: 13, y: 1, z: -15, time1: 2, time2: 32, 
+  rotateDuration: 0.8});
+createCylinder({x1: -10, x2: 10, y: 0.2, z: 0, time1: 10, time2: 20, 
+  rotateDuration: 4});
 
-// add the main animation (only if there are multiple keys)
-if (data.keys.length > 1) {
-  scene.animations[0].tracks.push({
-    name: `${id}.position`,
-    type: 'vector3',
-    keys: data.keys.map(key => ({ time: key.time * fps, value: [ ...key.value ]})),
-  });
+/**
+ * Create an animating cylinder.
+ *
+ * @param {Object} conf Configuration data.
+ */
+function createCylinder(conf) {
+  const { x1, x2, y, z, time1, time2, rotateDuration } = conf;
+  const id = uuidv4();
+  const data = {
+    id,
+    imageFile: 'matthaikirchplatz/crowface1.jpg',
+    keys: [
+      { time: time1, value: [x1, y, z]},
+      { time: time2, value: [x2, y, z]},
+    ],
+    modelFile: 'matthaikirchplatz.glb',
+    modelName: 'cylinder',
+  }
+  scene.external3DModels.push(data);
+  
+  // add the main animation (only if there are multiple keys)
+  if (data.keys.length > 1) {
+    scene.animations[0].tracks.push({
+      name: `${id}.position`,
+      type: 'vector3',
+      keys: data.keys.map(key => ({ time: key.time * fps, value: [ ...key.value ]})),
+    });
+  }
+  
+  const rotateKeys = [];
+  const angles = [-0.75, 0.75, -0.77];
+  for (let i = 0, n = angles.length; i < n; i++) {
+    const normal = i / (n - 1);
+    const angle = angles[i] * Math.PI;
+    const quaternion = new Quaternion().setFromEuler(new Euler(0, angle, 0)).normalize();
+    rotateKeys.push({
+      time: normal * rotateDuration * fps,
+      value: [ quaternion.x, quaternion.y, quaternion.z, quaternion.w ],
+    });
+  }
+  const animationClip = {
+    duration: rotateDuration,
+    fps,
+    loop: LoopRepeat,
+    name: `animation-${id}`,
+    tracks: [
+      {
+        name: `${id}.quaternion`,
+        keys: rotateKeys,
+        type: 'quaternion',
+      }
+    ],
+  };
+  scene.animations.push(animationClip);
+  
 }
-
-const duration = 2;
-const startTime = 0;
-const endTime = (startTime + duration) * fps;
-const rotateKeys = [];
-const angles = [-0.75, 0.75, -0.77];
-for (let i = 0, n = angles.length; i < n; i++) {
-  const normal = i / (n - 1);
-  const angle = angles[i] * Math.PI;
-  const quaternion = new Quaternion().setFromEuler(new Euler(0, angle, 0)).normalize();
-  rotateKeys.push({
-    time: startTime + (normal * (endTime - startTime)),
-    value: [ quaternion.x, quaternion.y, quaternion.z, quaternion.w ],
-  });
-}
-const animationClip = {
-  duration,
-  fps,
-  loop: LoopRepeat,
-  name: `animation-${id}`,
-  tracks: [
-    {
-      name: `${id}.quaternion`,
-      keys: rotateKeys,
-      type: 'quaternion',
-    }
-  ],
-};
-scene.animations.push(animationClip);
