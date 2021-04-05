115d7f0d55f4465c58bce6b75b1120abaac85cef
Wouter Hisschemöller <email@hisschemoller.com>
2021-04-05 18:58:49 +0200

    Two more people in front of the fountain.

--- public/js/data/plstmichel/plstmichel-actors1.js
+++ public/js/data/plstmichel/plstmichel-actors1.js
@@ -1,7 +1,7 @@
 import { getDefaultScene, getMatrix, fps, PREVIEW_SCALE, } from './plstmichel-shared.js';
 import createActor from '../../app/actor.js';
 
-const scene = getDefaultScene([0, 48 ], 'actors1', true);
+const scene = getDefaultScene([0, 50 ], 'actors1', true);
 export default scene;
 
 { // PAAR STIL LINKS
@@ -27,7 +27,7 @@ export default scene;
   createActor(scene, fps, {
     geom: { w: 3, h: 3, d: 0.5 },
     keys: [
-      { t:  0 + actorStart, v: [  15, 0, -10]},
+      { t:  0 + actorStart, v: [ 15, 0, -10]},
     ],
     matrix: getMatrix({ ry: Math.PI * 0, }).elements,
     canvas: { offset: 100, scale: 512/4, size: 512 },
@@ -202,6 +202,48 @@ export default scene;
     vKeys: [
       { t:  0 + actorStart, v: [ -100, 260]},
       { t:  6 + actorStart, v: [ 500, 260]},
+    ],
+    vrid: '1581_preview',
+  });
+}
+
+{ // MAN TELEFONEREND VAN RECHTS
+  const actorStart = 3;
+  createActor(scene, fps, {
+    geom: { w: 2, h: 3, d: 0.5 },
+    keys: [
+      { t:  0.01 + actorStart, v: [ 15, 0, -9]},
+      { t:  24 + actorStart, v: [ 27, 0, 12]},
+    ],
+    matrix: getMatrix({ ry: Math.PI * 0, }).elements,
+    canvas: { offset: 0, scale: 512/20, size: 512 },
+    vSc: 512/480,
+    vt: [121, 144],
+    vKeys: [
+      { t:  0 + actorStart, v: [ 365, 195], a: -0.9},
+      { t:  12 + actorStart, v: [ 250, 195], a: -0.98},
+      { t:  24 + actorStart, v: [ -130, 195 ]},
+    ],
+    vrid: '1581_preview',
+  });
+}
+
+{ // MAN VAN LINKS MET RODE TRUI
+  const actorStart = 22;
+  createActor(scene, fps, {
+    geom: { w: 2.5, h: 3, d: 1 },
+    keys: [
+      { t:  0 + actorStart, v: [ 30, -3.5, 20]},
+      { t:  0.01 + actorStart, v: [ 30, 0, 20]},
+      { t:  22 + actorStart, v: [ 15, 0, 40]},
+    ],
+    matrix: getMatrix({ ry: Math.PI * 0, }).elements,
+    canvas: { offset: 0, scale: 512/16, size: 512 },
+    vSc: 512/480,
+    vt: [0, 22],
+    vKeys: [
+      { t:  0 + actorStart, v: [ 25, 200], a: -0.8},
+      { t: 22 + actorStart, v: [ 380, 200]},
     ],
     vrid: '1581_preview',
   });
